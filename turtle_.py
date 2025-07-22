d078fd51c1e853d1fe8720f22781d2781db34d56
StIgorLeonSimf <aligol6573@gmail.com>
2025-07-22 22:39:11 +0300

    first

---
+++ turtle_.py
@@ -0,0 +1,28 @@
+from turtle import *
+
+pensize(4)
+shape('turtle')
+color('green')
+coord = [-200, 200, 0]
+fillcolor('yellow')
+for j in range(3):
+    begin_fill()
+    for i in range(3):
+        forward(100)
+        lt(120)
+    end_fill()
+    penup()
+    goto(coord[j], 0)
+    pendown()
+# for i in range(3):
+#     forward(100)
+#     lt(120)
+
+
+
+
+
+
+
+
+mainloop()
